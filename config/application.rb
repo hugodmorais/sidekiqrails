d437ac2a2128e788d2ea8de1c76db9973a3bb2d7
hugodmorais <hugodanielmorais@gmail.com>
2022-02-07 21:28:46 +0000

    Add sidekiq

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Sidekiqrails
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
+    config.active_job.queue_adapter = :sidekiq
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
